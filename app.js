703869f6bc42b0d002b96fe8f7007ee2b38f16c8
Alan Margolies <amargolies88@gmail.com>
2020-03-11 15:39:02 -0400

    unique id is added to each note

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+function findNewID() {
 
+}
 
 // API ROUTES
 app.get("/api/notes", function (req, res) {
@@ -39,14 +41,43 @@ app.get("*", function (req, res) {
 app.post("/api/notes", function (req, res) {
     console.log("api post req.bodeh:");
     console.log(req.body);
+    let newItem = req.body;
     let finalData = [];
 
     fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
         if (err) throw err;
 
-        //Parse the json data and push req.body onto array finalData
+        //Parse the json data
         finalData = JSON.parse(data);
-        finalData.push(req.body);
+
+        //look for available id and add to note property
+        console.log(`final data length: ${finalData.length}`)
+        if (finalData.length < 1) {
+            newItem.id = 1;
+        } else {
+            for (let i = 1; i < finalData.length + 2; i++) {
+                for (let j = 0; j < finalData.length; j++) {
+
+                    //if this id exists
+                    if (i === finalData[j].id) {
+                        break;
+                    }
+
+                    //on last iteration:
+                    if (j === finalData.length - 1) {
+                        console.log(`got here with id: ${i}`);
+                        newItem.id = i;
+                    }
+                }
+                if (newItem.id) {
+                    break;
+                }
+            }
+        }
+
+
+
+        finalData.push(newItem);
         let jsonData = JSON.stringify(finalData);
 
         //Save finalData as json in db.json
@@ -54,7 +85,7 @@ app.post("/api/notes", function (req, res) {
             if (err) throw err;
             console.log("wrote to database:");
             console.log(jsonData);
-            return req.body;
+            res.send(newItem);
         });
     });
 });
