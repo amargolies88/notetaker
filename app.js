616521c6bf7d8af541adfd10a87b3110404e487f
Alan Margolies <amargolies88@gmail.com>
2020-03-11 16:27:03 -0400

    delete button deletes notes

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-function findNewID() {
-
-}
-
 // API ROUTES
-app.get("/api/notes", function (req, res) {
+app.get("/api/notes", (req, res) => {
     // console.log("hello");
     console.log("api get req and res:")
     console.log(req.body);
@@ -25,10 +21,32 @@ app.get("/api/notes", function (req, res) {
         console.log(data);
         res.json(JSON.parse(data));
     });
-
-
-    // res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+app.delete("/api/notes/:id", (req, res) => {
+    console.log(req.params.id);
+    let idToDelete = parseInt(req.params.id);
+    let noteArray = [];
+
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+        if (err) throw err;
+        console.log(`note id to delete: ${idToDelete}`);
+        noteArray = JSON.parse(data);
+        console.log("old note array:");
+        console.log(noteArray);
+        noteArray = noteArray.filter(obj => obj.id !== idToDelete);
+        console.log(`new noteArray:`);
+        console.log(noteArray);
+        jsonNotes = JSON.stringify(noteArray);
+        fs.writeFile(path.join(__dirname, "/db/db.json"), jsonNotes, (err) => {
+            console.log(`wrote to db:`);
+            console.log(jsonNotes);
+            if (err) throw err;
+        });
+    });
+
+})
+
 
 // ROUTES
 app.get("/notes", function (req, res) {
@@ -58,30 +76,29 @@ app.post("/api/notes", function (req, res) {
             for (let i = 1; i < finalData.length + 2; i++) {
                 for (let j = 0; j < finalData.length; j++) {
 
-                    //if this id exists
+                    //if this id exists break this loop and move to next id to check
                     if (i === finalData[j].id) {
                         break;
                     }
 
-                    //on last iteration:
+                    //if this is the last iteration and we made it here by passing all previous checks then
+                    //the id is unique and we will use it
                     if (j === finalData.length - 1) {
-                        console.log(`got here with id: ${i}`);
                         newItem.id = i;
                     }
                 }
+                // if we set the id then we can stop checking for ids
                 if (newItem.id) {
                     break;
                 }
             }
         }
 
-
-
         finalData.push(newItem);
         let jsonData = JSON.stringify(finalData);
 
         //Save finalData as json in db.json
-        fs.writeFile(path.join(__dirname, "/db/db.json"), jsonData, (err) => {
+        fs.writeFile(path.join(__dirname, "/db/db.json"), jsonData, err => {
             if (err) throw err;
             console.log("wrote to database:");
             console.log(jsonData);
